eb72dbb9242b4eea00bf0c6a6504381876655fa6
이창섭 <ventulus95@gmail.com>
2022-12-18 17:10:24 +0900

    feature: 유저와 관련한 엔티티 추가
    - user, user_car, device 추가

---
+++ src/user/entities/device.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity("device")
+export class Device {
+  @PrimaryGeneratedColumn()
+  device_id: number;
+
+  @Column()
+  device_token: string;
+
+  @ManyToOne(() => User, {lazy: true})
+  @JoinColumn({
+    name: 'user_id',
+    referencedColumnName: 'user_id'
+  })
+  user: User;
+
+  
+
+}
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,8 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+  
+}
