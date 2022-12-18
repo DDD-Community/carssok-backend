d1577e1c67b98e70c36aec649bac94e407ec7867
이창섭 <ventulus95@gmail.com>
2022-12-18 17:11:40 +0900

    feat: user 모듈 추가
    - 유저 모듈 추가
    - 유저 서비스 추가
    - 유저 컨트롤러 추가

---
+++ src/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Device } from './entities/device.entity';
+import { User } from './entities/user.entity';
+import { UserCar } from './entities/user_car.entity';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Device, UserCar])],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService]
+})
+export class UserModule {}
