eb2e69d484d154a35d4f5062f36f82a28846aa41
이창섭 <ventulus95@gmail.com>
2022-12-28 17:44:12 +0900

    feat: middleware 정리

--- src/app.module.ts
+++ src/app.module.ts
@@ -38,6 +38,6 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(SimpleAuthMiddleware)
-      .forRoutes(HelloController)
+      .forRoutes('*')
   }
 }
