e3278bcaabb406e46a91efee31a23d1c7be7416a
이창섭 <ventulus95@gmail.com>
2022-12-29 15:21:53 +0900

    feat: Guard 기능 수정
    - Exception 대응

--- src/simple-auth/simple-auth.guard.ts
+++ src/simple-auth/simple-auth.guard.ts
@@ -1,26 +1,34 @@
-import { CanActivate, ExecutionContext, HttpException, Inject, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { UserService } from 'src/user/user.service';
 import { encryptionUtills } from 'src/utils/encryption';
 
 @Injectable()
 export class SimpleAuthGuard implements CanActivate {
-
   @Inject(UserService)
-  private readonly userService: UserService
+  private readonly userService: UserService;
 
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    if(!this.validateUser(request))
-      throw new HttpException("존재하지 않는 유저입니다.", 411);
-    return true;
+    try {
+      return this.validateUser(request);
+    } catch (error) {}
   }
 
   async validateUser(request: any): Promise<boolean> {
-    //FIXME 예외처리 잡아줘야함
-    const userId = parseInt(await encryptionUtills.decrypt(request.headers['user_token']))
-    return await this.userService.isUser(userId)
+    const userId = parseInt(
+      await encryptionUtills.decrypt(request.headers['user_token']),
+    );
+    if (Number.isNaN(userId))
+      throw new HttpException('존재하지 않는 유저입니다.', 411);
+    return await this.userService.isUser(userId);
   }
 }
