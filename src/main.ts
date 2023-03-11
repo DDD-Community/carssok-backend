db41cfda6f22c11a6c10bc59f2b9bbc12c4fe834
이창섭 <ventulus95@gmail.com>
2023-03-11 12:32:59 +0900

    feat: logging lib 셋업

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logger } from './utils/winston.util';
 
 async function bootstrap() {
-  console.log(process.env);
-  const app = await NestFactory.create(AppModule);
-  // app.useGlobalPipes(new ValidationPipe({transform: true}));
+  const app = await NestFactory.create(AppModule, {
+    logger: logger,
+  });
   await app.listen(3000);
 }
 bootstrap();
